49c0ee3a70b63894dbe5cd33d63be2b7d54d7069
Ravindra R. Jaju <jaju@msync.org>
2011-04-24 15:11:39 +0000

    Print some useful messages while running main...

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,18 +3,20 @@ import org.msync.datagen.numbers._
 
 object Main {
 
-  def printAFew[A](seq : SeqTrait[A], i : Int = 15) : Unit = {
+  def printAFew[A](seq : SeqTrait[A], i : Int = 5, msg : String = "Sequence") : Unit = {
+    println("**************** " + msg + "******************")
     for (counter <- 0 until i) {
       println(seq.next)
     }
     println("**************** DONE ******************")
+    println
   }
 
   def main(args : Array[String]) {
-    printAFew(new Integer())
-    printAFew(new RandomRangeInteger())
-    printAFew(new MonotonicRandomInteger())
-    printAFew(new PerturbedInteger())
-    printAFew(new org.msync.datagen.strings.String)
+    printAFew(new Integer(), msg = "Integers")
+    printAFew(new RandomRangeInteger(), msg = "Random Integers in a given range")
+    printAFew(new MonotonicRandomInteger(), msg = "Monotonically increasing (pseudo) random numbers")
+    printAFew(new PerturbedInteger(), msg = "Increasing integers perturbed within a range")
+    printAFew(new org.msync.datagen.strings.String(minLength = 5, maxLength = 10), msg = "Random strings with length between some range")
   }
 }
